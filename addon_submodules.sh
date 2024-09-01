58151763a9d529870581822b0cab95154a1fb98a
george <gavgeris@gmail.com>
2024-09-01 20:47:59 +0300

    - Merged changes for Moodle 4.4 - Αφαίρεση του Απο - Εως στις ενότητες - Διατλήρηση του hack για εμφάνιση σαν λίστα στο 2ο επίπεδο (δλδ μέσα στην ενότητα)

--- addon_submodules.sh
+++ addon_submodules.sh
@@ -1,10 +1,15 @@
+  GNU nano 4.8                                                                           addon_submodules.sh                                                                            Modified
 #! /bin/sh
 
 # mods
-git submodule add https://github.com/gavgeris/moodle-mod_certificate mod/certificate
+#git submodule add https://github.com/gavgeris/moodle-mod_certificate mod/certificate
+
 git submodule add https://github.com/learnweb/moodle-mod_groupmembers mod/groupmembers
 git submodule add https://github.com/PoetOS/moodle-mod_questionnaire mod/questionnaire
 git submodule add https://github.com/moodleworkplace/moodle-mod_coursecertificate mod/coursecertificate
+git submodule add https://github.com/h5p/moodle-mod_hvp mod/hvp
+git submodule add https://gavgeris@bitbucket.org/gavgeris/vmachine.git mod/vmachine
+
 
 #blocks
 git submodule add https://github.com/jonof/moodle-block_completion_progress blocks/completion_progress
@@ -30,6 +35,10 @@ git submodule add https://github.com/ndunand/moodle-atto_morebackcolors lib/edit
 git submodule add https://github.com/moodle-an-hochschulen/moodle-atto_styles lib/editor/atto/plugins/styles
 git submodule add https://github.com/sharpchi/moodle-atto_templates4u lib/editor/atto/plugins/templates4u
 git submodule add https://github.com/rogersegu/moodle-atto_c4l/ lib/editor/atto/plugins/c4l
+git submodule add https://github.com/ecampbell/moodle-atto_wordimport lib/editor/atto/plugins/wordimport
+
+#tinymce plugins
+git submodule add https://github.com/reskit/moodle-tiny_c4l lib/editor/tiny/plugins/tiny_c4l
 
 
 #tools
@@ -37,10 +46,16 @@ git submodule add https://github.com/doiphode/moodle-tool_clearbackupfiles admin
 git submodule add https://github.com/moodle-an-hochschulen/moodle-tool_redis admin/tool/redis
 git submodule add https://github.com/moodleworkplace/moodle-tool_certificate admin/tool/certificate
 
+#availability
+git submodule add https://github.com/ewallah/moodle-availability_coursecompleted availability/condition/coursecompleted
+git submodule add https://github.com/moodle-an-hochschulen/moodle-availability_role availability/condition/role
+
 #course formats
-git submodule add https://github.com/cellule-tice/moodle-format_collapsibletopics course/format/collapsibletopics
+#git submodule add https://github.com/cellule-tice/moodle-format_collapsibletopics course/format/collapsibletopics
 git submodule add https://github.com/gavgeris/moodle-format_remuiformat.git course/format/remuiformat
+#git submodule add https://github.com/WisdmLabs/moodle-format_remuiformat course/format/remuiformat
 git submodule add https://github.com/gjb2048/moodle-format_topcoll.git course/format/topcoll
+git submodule add https://github.com/marinaglancy/moodle-format_flexsections course/format/flexsections
 
 #themes
 git submodule add https://gitlab.com/jezhops/moodle-theme_adaptable.git theme/adaptable
@@ -52,6 +67,7 @@ git submodule add https://github.com/gavgeris/moodle-theme_etwinning theme/etwin
 git submodule add https://github.com/moodle-an-hochschulen/moodle-local_boostnavigation local/boostnavigation
 git submodule add https://gitlab.com/adapta/moodle-local_modcustomfields local/modcustomfields
 git submodule add https://github.com/moodle-an-hochschulen/moodle-local_staticpage local/staticpage
+git submodule add https://github.com/gavgeris/local_reusecontent_ws.git local/reusecontent_ws
 
 #enrolment
 git submodule add https://github.com/bobopinna/moodle-enrol_autoenrol enrol/autoenrol
