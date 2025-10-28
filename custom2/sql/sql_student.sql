a146409e28f03b77811de79154eb45f3c3d6afe6
George Avgeris <gavgeris@gmail.com>
2025-10-28 18:09:01 +0200

    Optimize teacher's query

--- custom2/sql/sql_student.sql
+++ custom2/sql/sql_student.sql
@@ -10,7 +10,7 @@ FROM
   mdl_course_categories mcc,
   mdl_groups mg,
   mdl_groups_members mgm
-WHERE mcc.name LIKE '2022-23'
+WHERE mcc.name LIKE '2025-26'
   AND mc.category = mcc.id
   AND ma.course = mc.id 
   AND mas.assignment = ma.id 
--- custom2/sql/sql_teacher.sql
+++ custom2/sql/sql_teacher.sql
@@ -1,68 +1,28 @@
-SELECT
+Select
     CONCAT('<a target="_blank" href="',
            'https://seminars.etwinning.gr/mod/assign/view.php&quest;id=',(SELECT id FROM mdl_course_modules mcm WHERE instance = ma.id AND course = mc.id AND module = 1 LIMIT 1),'&action=grading">',
            CONCAT(CONCAT(LPAD(ROUND(DATEDIFF(FROM_UNIXTIME(allowsubmissionsfromdate), FROM_UNIXTIME(mc.startdate)) / 7) + 1,2,'0'), " - "), ma.name),
            '</a>'
-        ) AS assignment,
-    COUNT(distinct mgm.userid) AS plithos
+    ) AS assignment,
+    COUNT(distinct ge.epimorfoumenos) AS plithos
 FROM
-    mdl_assign_submission mas,
-    mdl_assign ma,
-    mdl_course mc,
-    mdl_groups mg,
-    mdl_groups_members mgm,
-    (SELECT
-         mg.courseid,
-         b.firstname,
-         b.lastname,
-         b.id,
-         mg.name AS groupname,
-         mg.id AS groupid,
-         email
-     FROM
-         mdl_role_assignments a,
-         mdl_user b,
-         mdl_context c,
-         mdl_groups mg,
-         mdl_groups_members mgm
-     WHERE a.roleid IN (5)
-       AND a.userid = b.id
-       AND a.contextid = c.id
-       AND mg.id = mgm.groupid
-       AND mgm.userid = a.userid
-       AND b.id IN (
-         SELECT DISTINCT epimorfoumenos
-         FROM group_epimorfwth
-         WHERE courseid = ?
-           AND epimorfwths = ?
-           and groupname != 'Επιμορφωτές'
-       AND groupname != 'Συντονιστές'
-    )
-    AND mg.courseid = c.instanceid
-       AND mg.courseid = ?) table1
-WHERE ma.course = mc.id
-  AND mas.assignment = ma.id
+    mdl_course mc JOIN group_epimorfwth ge ON (ge.courseid = mc.id AND epimorfwths = ?)
+                  JOIN mdl_assign ma ON (ma.course = mc.id )
+                  JOIN mdl_assign_submission mas ON (mas.assignment = ma.id AND mas.userid = ge.epimorfoumenos)
+where mc.id = ?
   AND mas.status = 'submitted'
-  AND mgm.userid = mas.userid
+  AND ge.groupname != 'Επιμορφωτές' AND ge.groupname != 'Συντονιστές'
   AND (grade != 2 OR grade IS NULL)
-  AND mas.userid != table1.id -- Εξαιρώ τις εργασίες που εχει υποβάλλει ο επιμορφωτής (αν ήταν πριν επιμορφούμενος)
-
-  AND mg.courseid = mc.id
-  AND mg.id = mgm.groupid
-
-  AND table1.courseid = mc.id
-  AND table1.groupid = mg.id
 
   AND ( NOT EXISTS (SELECT 1
-    FROM mdl_assign_grades mag
-    WHERE mag.assignment = mas.assignment
-  AND userid = mas.userid)
-   OR (mas.timemodified >= (SELECT MAX(mag.timemodified)
-    FROM mdl_assign_grades mag
-    WHERE mag.assignment = mas.assignment
-  AND userid = mas.userid
-    ))
-    )
+                    FROM mdl_assign_grades mag
+                    WHERE mag.assignment = mas.assignment
+                      AND userid = mas.userid)
+    OR (mas.timemodified >= (SELECT MAX(mag.timemodified)
+                             FROM mdl_assign_grades mag
+                             WHERE mag.assignment = mas.assignment
+                               AND userid = mas.userid
+    )))
 GROUP BY assignment
 UNION
 SELECT
@@ -70,67 +30,32 @@ SELECT
            'http://seminars.etwinning.gr/mod/forum/view.php&quest;id=',(SELECT id FROM mdl_course_modules mcm WHERE instance = mf.id AND course = mc.id AND module = 9 LIMIT 1),'">',
            'Forum:', mf.name,
            '</a>'
-        ) AS assignment,
+    ) AS assignment,
     COUNT(distinct mfp.userid) AS plithos
 FROM
-    mdl_course mc,
-    mdl_groups mg,
-    mdl_groups_members mgm,
+    mdl_course mc JOIN group_epimorfwth ge ON (ge.courseid = mc.id AND epimorfwths = ?),
     mdl_forum mf,
     mdl_forum_discussions mfd,
-    mdl_forum_posts mfp,
-    (SELECT
-         mg.courseid,
-         b.firstname,
-         b.lastname,
-         b.id as userid,
-         mg.name AS groupname,
-         mg.id AS groupid,
-         email
-     FROM
-         mdl_role_assignments a,
-         mdl_user b,
-         mdl_context c,
-         mdl_groups mg,
-         mdl_groups_members mgm
-     WHERE a.roleid IN (5)
-       AND a.userid = b.id
-       AND a.contextid = c.id
-       AND mg.id = mgm.groupid
-       AND mgm.userid = a.userid
-       AND b.id IN (
-         SELECT DISTINCT epimorfoumenos
-         FROM group_epimorfwth
-         WHERE courseid = ?
-           AND epimorfwths = ?
-           and groupname != 'Επιμορφωτές'
-       AND groupname != 'Συντονιστές'
-    )
-    AND mg.courseid = c.instanceid
-       AND mg.courseid = ?) table1
-WHERE mg.courseid = mc.id
-  AND mgm.groupid  = mg.id
+    mdl_forum_posts mfp
 
-  AND mf.course = mg.courseid
+WHERE mc.id = ?
+  AND mf.course = mc.id
   AND mfd.forum = mf.id
   AND mfd.id = mfp.discussion
 
-  AND mgm.userid = mfp.userid
+  AND mfp.userid = ge.epimorfoumenos
   AND mf.assessed != 0
-
-  AND table1.courseid = mc.id
-  AND table1.groupid = mg.id
-  AND table1.userid = mfp.userid -- User in Teacher's Group
 
   AND mfp.userid NOT IN (
     SELECT mgg.userid
     FROM mdl_grade_items mgi, mdl_grade_grades mgg
-    WHERE courseid = mg.courseid
-  AND mgi.id = mgg.itemid
-  AND itemmodule = 'forum'
-  AND finalgrade IS NOT NULL
-  AND iteminstance = mf.id
-    )
-GROUP BY assignment
--- ORDER BY LPAD(ROUND(DATEDIFF(FROM_UNIXTIME(allowsubmissionsfromdate), FROM_UNIXTIME(mc.startdate)) / 7) + 1,2,'0'), plithos desc
+    WHERE courseid = mc.id
+      AND mgi.id = mgg.itemid
+      AND itemmodule = 'forum'
+      AND finalgrade IS NOT NULL
+      AND iteminstance = mf.id
+)
+GROUP BY assignment;
 
+
+;
